7b266e60a14f0473c529c280382491413c348a1b
Roger <rogerluk93@gmail.com>
2014-11-20 18:10:56 -0800

    clerk updated
    Signed-off-by: Roger <rogerluk93@gmail.com>

---
+++ clerk.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<title>Clerk</title>
+
+<body>Item return</body>
+
+<p>Enter information for returned item</p>
+<form method="POST" action="home.php">
+<table border=0 cellpadding=0 cellspacing=0>
+<tr><td>returnId</td><td><input type="text" size=20 name="new_retid"</td></tr>
+<tr><td>Date</td><td><input type="text" size=20 name="new_date"</td></tr>
+<tr><td>receiptId</td><td><input type="text" size=20 name="receiptId"</td></tr>
+<tr><td>Item upc</td><td><input type="text" size=20 name="upc"</td></tr>
+<tr><td>Quantity</td><td><input type="text" size=20 name="new_quantity"</td></tr>
+<tr><td></td><td><input type="submit" name="submit" value="submit"></td></tr>
+</table>
+</form>
