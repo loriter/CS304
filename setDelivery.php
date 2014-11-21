70f027cd4d1335f4a85ae1c81181100bec9eeef3
Roger <rogerluk93@gmail.com>
2014-11-20 17:53:55 -0800

    Something something delivery

---
+++ setDelivery.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<title>setDelivery</title>
+<link href="bookbiz.css" rel="stylesheet" type="text/css">
+
+<body>Set delivery date</body>
+
+<p>Update the delivery date of an order</p>
+<form method="POST" action="manager.php">
+<table border=0 cellpadding=0 cellspacing=0>
+<tr><td>receiptID</td><td><input type="text" size=20 name=receiptID</td></tr>
+<tr><td>DeliveryDate</td><td><input type="text" size=20 name="new_DeliveryDate"</td></tr>
+<tr><td></td><td><input type="submit" name="submit" value="submit"></td></tr>
+</table>
+</form>
