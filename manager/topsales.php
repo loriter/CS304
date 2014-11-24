ab7cc9d000e7c4d58c263384e610b8cfb22f767f
Roger <rogerluk93@gmail.com>
2014-11-24 14:03:01 -0800

    top sales? yeah top sales.
    Signed-off-by: Roger <rogerluk93@gmail.com>

---
+++ manager/topsales.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<title>Top Sales</title>
+
+<body>Top Sales</body>
+
+<p>Input date and top sales</p>
+<form method="POST" action="topsalesdisplay.php">
+<table border=0 cellpadding=0 cellspacing=0>
+<tr><td>Date</td><td><input type="text" size=30 name="new_cid"</td></tr>
+<tr><td>Number of Sales</td><td><input type="text" size=30 name="new_numsales"</td></t
+<tr><td></td><td><input type="submit" name="Search" value="Search"></td></tr>
+</table>
+</form>
+
+
+<p>Back to manager home page</p>
+<form method="POST" action="manager.php">
+<p><input type="submit" value="Back" name="Back"></p>
+</form>
