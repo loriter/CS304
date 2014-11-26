61f972f2f8de17ba88084f45a4b5db694778dfb2
Roger <rogerluk93@gmail.com>
2014-11-26 11:51:17 -0800

    44444444444444444444

--- manager/report1.php
+++ manager/report1.php
@@ -16,18 +16,18 @@ All sales for: <?php echo $_POST["new_rdate"]; ?>
 <td class=rowheader>Unit Price</td>
 <td class=rowheader>Units</td>
 <td class=rowheader>TotalIValue</td>
-</tr> 
-
+<td class=rowheader>TotalCValue</td>
+</tr>
 
 <?php
 // in this code here we have to populate the table with the correct stuff=
-$dadate = $_POST["new_rdate"];  // this holds the date variable 
+$dadate = $_POST["new_rdate"];  // this holds the date variable  
 $tValue = 0;
-/*$user = 'root'; 
+$cVal = 0;
+$user = 'root'; 
 $pass = ''; 
 $db = 'test';
-$connection = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");*/
-$connection = new mysqli("dbserver.ugrad.cs.ubc.ca", "j4n8", "a29454113", "j4n8");
+$connection = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
 
 if(!$rresult = $connection->query("SELECT I.upc, I.category, I.price, SUM(P.quantity) AS rsold, O.date
 FROM Item I, Purchase_Item P, Orders O
@@ -37,10 +37,22 @@ ORDER BY I.category, rsold")){
   echo "this query did not work1!!";
   die('There was an error running the report query');
 }
+/*
+$rrow = $rresult->fetch_assoc();
+echo "'.$rrow['category'].'";
+$cat = $rrow['category'];*/
+$a = 0;
+$b = 0;
 while($rrow = $rresult->fetch_assoc()){
+  if($a=$b){
+    $cat = $rrow['category'];
+    $b++;
+  }
+  if($cat == $rrow['category']){
     $rprice = $rrow['price'];
     $rquantity = $rrow['rsold'];
     $totalIVal = $rquantity * $rprice;
+    $cVal += $totalIVal;
     $tValue += $totalIVal;
     echo "<tr><td>".$rrow['upc']."</td>"; 
     echo "<td>".$rrow['category']."</td>";  
@@ -48,9 +60,28 @@ while($rrow = $rresult->fetch_assoc()){
     echo "<td>".$rrow['rsold']."</td>";
     echo "<td>".$totalIVal."</td></tr>"; 
   }
+  else{
+    echo "<tr><td>".$rrow['upc']."</td>"; 
+    echo "<td>".$rrow['category']."</td>";  
+    echo "<td>".$rrow['price']."</td>";
+    echo "<td>".$rrow['rsold']."</td>";
+    echo "<td>".$totalIVal."</td></tr>"; 
+    echo "<td>".$cVal."</td></tr>";
+    $cVal = 0;
+  }  
+    $cat = $rrow['category'];
+}
+
+    echo "<tr><td>".$rrow['upc']."</td>"; 
+    echo "<td>".$rrow['category']."</td>";  
+    echo "<td>".$rrow['price']."</td>";
+    echo "<td>".$rrow['rsold']."</td>";
+    echo "<td>".$totalIVal."</td></tr>"; 
+    echo "<td>".$cVal."</td></tr>";
+    echo "</tbody></table>";
 ?>
 <tbody>
-<tr valign=right>
+<tr valign=center>
 <td class=rowheader>TValue</td>
 </tr>
 <?php    
@@ -60,5 +91,6 @@ while($rrow = $rresult->fetch_assoc()){
 
 <form method="POST" action="report.php">
 <p><input type="submit" value="Back" name="Back"></p>
+</form> 
 </html>
 
