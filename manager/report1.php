6ae805d7d66e80963a40ad6d08f428f57f0a3619
loriter <mingalinglee@gmail.com>
2014-11-26 12:14:36 -0800

    Daily Sales report completed

--- manager/report1.php
+++ manager/report1.php
@@ -15,77 +15,53 @@ All sales for: <?php echo $_POST["new_rdate"]; ?>
 <td class=rowheader>Category</td>
 <td class=rowheader>Unit Price</td>
 <td class=rowheader>Units</td>
-<td class=rowheader>TotalIValue</td>
-<td class=rowheader>TotalCValue</td>
+<td class=rowheader>TotalValue</td>
 </tr>
 
 <?php
-// in this code here we have to populate the table with the correct stuff=
-$dadate = $_POST["new_rdate"];  // this holds the date variable  
-$tValue = 0;
-$cVal = 0;
-$user = 'root'; 
-$pass = ''; 
-$db = 'test';
-$connection = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
+	// in this code here we have to populate the table with the correct stuff=
+	$dadate = $_POST["new_rdate"];  // this holds the date variable  
+	$user = 'j4n8'; 
+	$pass = 'a29454113'; 
+	$db = 'j4n8';
+	$connection = new mysqli('dbserver.ugrad.cs.ubc.ca', $user, $pass, $db);
 
-if(!$rresult = $connection->query("SELECT I.upc, I.category, I.price, SUM(P.quantity) AS rsold, O.date
-FROM Item I, Purchase_Item P, Orders O
-WHERE O.date = $dadate AND I.upc = P.upc AND P.receiptId = O.receiptId
-GROUP BY I.title, I.company, O.date
-ORDER BY I.category, rsold")){
-  echo "this query did not work1!!";
-  die('There was an error running the report query');
-}
-/*
-$rrow = $rresult->fetch_assoc();
-echo "'.$rrow['category'].'";
-$cat = $rrow['category'];*/
-$a = 0;
-$b = 0;
-while($rrow = $rresult->fetch_assoc()){
-  if($a=$b){
-    $cat = $rrow['category'];
-    $b++;
-  }
-  if($cat == $rrow['category']){
-    $rprice = $rrow['price'];
-    $rquantity = $rrow['rsold'];
-    $totalIVal = $rquantity * $rprice;
-    $cVal += $totalIVal;
-    $tValue += $totalIVal;
-    echo "<tr><td>".$rrow['upc']."</td>"; 
-    echo "<td>".$rrow['category']."</td>";  
-    echo "<td>".$rrow['price']."</td>";
-    echo "<td>".$rrow['rsold']."</td>";
-    echo "<td>".$totalIVal."</td></tr>"; 
-  }
-  else{
-    echo "<tr><td>".$rrow['upc']."</td>"; 
-    echo "<td>".$rrow['category']."</td>";  
-    echo "<td>".$rrow['price']."</td>";
-    echo "<td>".$rrow['rsold']."</td>";
-    echo "<td>".$totalIVal."</td></tr>"; 
-    echo "<td>".$cVal."</td></tr>";
-    $cVal = 0;
-  }  
-    $cat = $rrow['category'];
-}
+	if(!$rresult = $connection->query("SELECT I.upc, I.category, I.price, SUM(P.quantity) AS rsold, O.date
+	FROM item I, purchase_item P, orders O
+	WHERE O.date = '$dadate' AND I.upc = P.upc AND P.receiptId = O.receiptId
+	GROUP BY I.title, I.company, O.date
+	ORDER BY I.category, rsold")){
+		die('There was an error running the report query');
+		}
 
-    echo "<tr><td>".$rrow['upc']."</td>"; 
-    echo "<td>".$rrow['category']."</td>";  
-    echo "<td>".$rrow['price']."</td>";
-    echo "<td>".$rrow['rsold']."</td>";
-    echo "<td>".$totalIVal."</td></tr>"; 
-    echo "<td>".$cVal."</td></tr>";
-    echo "</tbody></table>";
-?>
-<tbody>
-<tr valign=center>
-<td class=rowheader>TValue</td>
-</tr>
-<?php    
-    echo "<td>".$tValue."</td></tr>";
+	$cat = null;
+	$cVal = 0;
+	$tVal = 0;
+
+	while($rrow = $rresult->fetch_assoc()){
+	
+		if($cat != $rrow['category']){
+			if($cat != null) {
+				echo "<tr><td></td><td>Total</td><td></td><td></td><td>".$cVal."</td></tr>";
+			}
+			$cat = $rrow['category'];
+			$cVal =0;
+		}	
+			
+		$rprice = $rrow['price'];
+		$rquantity = $rrow['rsold'];
+		$itemVal = $rquantity * $rprice;
+		$cVal += $itemVal;
+		$tVal += $itemVal;
+		
+		echo "<tr><td>".$rrow['upc']."</td>"; 
+		echo "<td>".$rrow['category']."</td>";  
+		echo "<td>".$rrow['price']."</td>";
+		echo "<td>".$rrow['rsold']."</td>";
+		echo "<td>".$itemVal."</td></tr>"; 
+	}
+	echo "<tr><td></td><td>Total</td><td></td><td></td><td>".$cVal."</td></tr>";
+	echo "<tr><td></td><td>Total Sales</td><td></td><td></td><td>".$tVal."</td></tr>";
     echo "</tbody></table>";
 ?>
 
